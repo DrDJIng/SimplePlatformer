a678f0623fd0821ede389706290dfc5ae763b909
David Ing <daverinoe@gmail.com>
2021-01-19 12:05:30 +0100

    Removed debugging code
    Removed a slight bit of debugging code.

--- Town.gd
+++ Town.gd
@@ -18,6 +18,5 @@ func _ready():
 func _on_Area2D_body_shape_entered(body_id, body, body_shape, area_shape):
 	# If the body entering the collision shape is the character collision shape, 
 	# character should die.
-	print(body)
 	if body_id == 1751:
 		PlayerVariables.shouldDie = 1
